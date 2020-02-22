cbfb254f78760e9f26c86d86c5e6a8d5ece5b407
Mgrdich <mgotm13@gmail.com>
2020-02-22 22:02:24 +0400

    Login + Logout

---
+++ client/src/components/auth/Login.tsx
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react';
+import {RouteComponentProps} from "react-router";
+
+
+const Login: React.FC<RouteComponentProps> = (props) => {
+
+    return (
+        <>
+            <h1>Login</h1>
+
+
+
+        </>
+    );
+};
+
+export default Login;
---
+++ client/src/components/auth/Register.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {RouteComponentProps} from "react-router";
+
+const Register: React.FC<RouteComponentProps>= (props) => {
+
+
+    return (
+        <>
+            <h1>Register</h1>
+
+        </>
+    );
+};
+
+export default Register;
