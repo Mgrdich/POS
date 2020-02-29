51e467cc54c54419b46b975026c3bda56835bcd3
sarkis1231 <sakooghly@gmail.com>
2020-02-29 23:36:40 +0400

    FE: added DynamicField

--- client/src/components/Reusable/DynamicFields.tsx
+++ client/src/components/Reusable/DynamicFields.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import {InputField} from "../../interfaces/General";
-import DropDown from "./DropDown";
-import {TextField} from "@material-ui/core";
 import {IDynamicFields} from "../../interfaces/Reusable";
-import {errorChecker, errorText} from "../../util/views";
-import PasswordField from "./PasswordField";
+import DynamicField from "./DynamicField";
 
 
 const DynamicFields: React.FC<IDynamicFields> = (props) => {
@@ -12,52 +9,10 @@ const DynamicFields: React.FC<IDynamicFields> = (props) => {
     return (
         <>
             {
-                props.InputFields.map((item: InputField, index: number) => {
-                    if (item.type === 'select') {
-                        return (
-                            <DropDown
-                                defaultValue={item.default}
-                                id={(item.id) ? item.id : item.name}
-                                label={item.placeholder}
-                                url={(item.url) ? item.url : ''}
-                                name={item.name}
-                                key={index}
-                                data={(item.data) ? item.data : []}
-                                control={control}
-                                error={errorChecker(item.name, errors, serverError)}
-                                helperText={errorText(item.name, errors, serverError)}
-                            />
-                        )
-                    } else if (item.type === 'password') {
-                        return (
-                            <PasswordField
-                                label={item.placeholder}
-                                id={(item.id) ? item.id : item.name}
-                                name={item.name}
-                                key={index}
-                                inputRef={item.validation ? register(item.validation) : register}
-                                error={errorChecker(item.name, errors, serverError)}
-                                helperText={errorText(item.name, errors, serverError)}
-                            />
-                        );
-                    } else {
-                        return (
-                            <TextField
-                                defaultValue={item.default}
-                                key={index}
-                                id={item.name}
-                                name={item.name}
-                                label={item.placeholder}
-                                variant="outlined"
-                                color="primary"
-                                inputRef={item.validation ? register(item.validation) : register}
-                                rows={(item.type === 'textArea') ? 4 : 1}
-                                error={errorChecker(item.name, errors, serverError)}
-                                helperText={errorText(item.name, errors, serverError)}
-                            />
-                        )
-                    }
-                })
+                props.InputFields.map((item: InputField, index: number) => (
+                <DynamicField key={index} item={item} index={index} errors={errors} register={register} serverError={serverError} control={control}/>
+
+                ))
             }
         </>
     );
