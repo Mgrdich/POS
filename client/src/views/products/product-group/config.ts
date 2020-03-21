dda4a61b01a17ff92e2973433fcb599fc6152149
hovaroyan <hovaroyan@gmail.com>
2020-03-21 23:04:29 +0400

    FE: added inputField to productGroup

---
+++ client/src/views/products/product-group/config.ts
@@ -0,0 +1,24 @@
+import * as yup from "yup";
+
+interface IProductGroup {
+    name: string;
+    placeholder: string;
+    type?: string;
+    url?: string;
+    ignoreNone?:boolean;
+
+};
+
+
+export const productGroupInputField: Array<IProductGroup> = [
+    {
+        name: 'name',
+        placeholder: 'Product group name',
+    }
+];
+
+
+
+export const productGroupValSchema = yup.object().shape({
+    name: yup.string().required(),
+});
--- client/src/views/products/product-group/index.tsx
+++ client/src/views/products/product-group/index.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import {useForm} from "react-hook-form";
+import {useServerErrorHandle} from "../../../components/Hooks/useServerErrorHandle";
+import {useDynamicFields} from "../../../components/Hooks/useDynamicFields";
+import axios from "axios";
+import {IAlertAxiosResponse} from "../../../interfaces/General";
+import Grid from "@material-ui/core/Grid";
+import DynamicFields from "../../../components/Reusable/DynamicFields";
+import {Button} from "@material-ui/core";
+import {productGroupInputField, productGroupValSchema} from "./config";
 
 const ProductGroup = () => {
+    const {handleSubmit, register, errors, control, unregister, reset} = useForm<any>({
+        validationSchema: productGroupValSchema,
+    });
+    const [serverError, setterError, resetServerError] = useServerErrorHandle();
+    useDynamicFields(productGroupInputField, register, unregister);
+
+    const onSubmit = function (values: any): void {
+        console.log(values);
+    };
     return (
-        <div>
-            Product Group
-        </div>
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Grid container direction="row" spacing={1}>
+                    <DynamicFields
+                        InputFields={productGroupInputField}
+                        register={register}
+                        errors={errors}
+                        control={control}
+                        serverError={serverError}
+                        Component={Grid}
+                        ComponentProps={
+                            {
+                                item: true,
+                                xs: 12,
+                                sm: 6,
+                                md: 4,
+                                lg: 3,
+                            }
+                        }
+                    />
+
+                </Grid>
+                <Button
+                    color="primary"
+                    variant="contained"
+                    size="large"
+                    className="FloatRight"
+                    type="submit"
+                >Submit</Button>
+            </form>
+        </>
     );
 };
 
