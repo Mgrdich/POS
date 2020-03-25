83899e14365559630021ffb00232275ebf0acaf9
sarkis1231 <sakooghly@gmail.com>
2020-03-25 19:56:41 +0400

    FE:added table in create product

--- client/src/views/products/config.ts
+++ client/src/views/products/config.ts
@@ -29,9 +29,32 @@ export const addProductInputField: Array<IAddProductInputField> = [
     },
 ];
 
+export const editProductInputField: Array<IAddProductInputField> = [
+    {
+        name: 'name',
+        placeholder: 'Product name',
+    },
+    {
+        name: 'price',
+        placeholder: 'Product price',
+    },
+    {
+        name: 'productsGroup',
+        placeholder: 'Product Groups',
+        type: 'select',
+        url: '/api/products-group',
+        ignoreNone:false,
+
+    },
+];
 
 
 export const addProductValSchema = yup.object().shape({
+    name: yup.string().required(),
+    price: yup.string().test('price', 'Invalid Value', (value) =>  !isNaN(parseInt(value)) ? value : null),
+    productsGroup:yup.string()
+});
+export const editProductValSchema = yup.object().shape({
     name: yup.string().required(),
     price: yup.string().test('price', 'Invalid Value', (value) =>  !isNaN(parseInt(value)) ? value : null),
     productsGroup:yup.string()
