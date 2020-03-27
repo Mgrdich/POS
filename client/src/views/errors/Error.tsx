574ad57d438195798218e99a513b6c06dd836160
sarkis1231 <sakooghly@gmail.com>
2020-03-28 01:30:36 +0400

    FE: designed the error message + code refactoring

--- client/src/views/errors/Error.tsx
+++ client/src/views/errors/Error.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import {IError} from "../../interfaces/Views/Errors";
+import {Button} from "@material-ui/core";
+import {useHistory} from "react-router";
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
 
 const Error: React.FC<IError> = (props) => {
+    const history = useHistory();
+
     return (
-        <div className={`errorPage errorPage${props.errorNumber}`}>
-            <h1>Error{props.errorNumber}</h1>
-            <p>{props.errorText}</p>
+        <div className='error'>
+            <div className='head'>
+                <h1>Error {props.errorNumber}</h1>
+                <div className={`errorPage errorPage${props.errorNumber}`}>
+                    <div className='icon-wrapper'>
+                    <p>{props.errorText}</p>
+                    <SentimentVeryDissatisfiedIcon/>
+                    </div>
+                </div>
+            </div>
+
+            <div className='bod'>
+                <Button color="primary" variant="outlined" onClick={()=> history.push('/')}>Home Page</Button>
+            </div>
+
         </div>
     );
 };
