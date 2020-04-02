69665f96fe7bf37fd3a8e7ea4fc99ad97a95b7b4
Mgo <mgotm13@gmail.com>
2020-04-02 22:58:20 +0400

    FE: POS Components

---
+++ client/src/views/pos/Products.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products:React.FC = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default Products;
---
+++ client/src/views/pos/TableOrders.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TableOrders:React.FC = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default TableOrders;
