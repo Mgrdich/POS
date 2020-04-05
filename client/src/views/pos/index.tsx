34bab2fc22dff3605de9e154b7d26ed7fd95ae1d
hovaroyan <hovaroyan@gmail.com>
2020-04-05 20:47:23 +0400

    FE: added onclick functionality to pos

--- client/src/views/pos/index.tsx
+++ client/src/views/pos/index.tsx
@@ -2,20 +2,24 @@ import React, {useEffect} from 'react';
 import {Grid} from "@material-ui/core";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchTables} from "../../actions/posActions";
+import {useHistory} from "react-router";
 
 const Pos:React.FC = () => {
     const data=[{name:"hov" , id:0 , number: 1}, {name:"sako" , id:1 , number: 2}, {name:"mgo" , id:2 , number: 3}];
     const tables = useSelector<any>(state => state.pos.tables);
     const dispatch = useDispatch();
+    const history = useHistory();
 
     useEffect(function () {
         dispatch(fetchTables());
     },[dispatch]);
 
-    console.log(tables); //TODO Hovik Table Number should be Link ask Sako to redirect
+    console.log(tables);
 
     return (
-        <Grid className="pos-tables" container direction="row" justify="space-around" alignContent="flex-start" wrap='wrap'>
+        <Grid className="pos-tables" container direction="row" justify="space-around" alignContent="flex-start" wrap='wrap'
+        onClick={()=>{(history.push("/tableorder"))}}
+        >
             {data.map((table)=>(
                 <Grid className="tables" item key={table.id}>
                     <span>{table.number}</span>
