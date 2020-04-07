02ff2a4d09684632329f99a678cd1508482567b3
hovaroyan <hovaroyan@gmail.com>
2020-04-07 12:59:52 +0400

    FE: added buttons to table order in pos

--- client/src/views/pos/TableOrders.tsx
+++ client/src/views/pos/TableOrders.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import {Paper} from "@material-ui/core";
+import {Button, Paper} from "@material-ui/core";
 
 const TableOrders: React.FC = () => {
+
     return (
         <div className="table-order-container">
         <Paper className="products-paper">
             <div className="products-header">
                 <h1>Table order</h1>
             </div>
+
             <div className="order-container">
                 <span> Name</span> <span>Quantity</span> <span>Price</span>
             </div>
-        </Paper>
+            <div className="order-button-container">
+                <Button
+                variant="outlined"
+                color="primary"
+                type="button"
+                > cancel </Button>
+
+                <Button
+                    variant="outlined"
+                    color="primary"
+                    type="button"
+                > submit </Button>
+            </div>
+            </Paper>
         </div>
     );
 };
