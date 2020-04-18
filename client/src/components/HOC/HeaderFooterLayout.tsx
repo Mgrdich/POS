e27d7ec70be981e2a22fa205af8cce6a830891e0
hovaroyan <hovaroyan@gmail.com>
2020-04-19 03:20:25 +0400

    FE: removed a warning

--- client/src/components/HOC/HeaderFooterLayout.tsx
+++ client/src/components/HOC/HeaderFooterLayout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import MenuDrawer from "../layout/Drawer/MenuDrawer";
-import Footer from "../layout/Footer";
 
 export function HeaderFooterLayout<P extends object>(WrappedComponent: React.ComponentType<P>):React.FC<P> {
     return function (props:P) {
