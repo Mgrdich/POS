895053396c3c6f300446eb5be2f31965e2830f77
Mgo <mgotm13@gmail.com>
2020-04-20 23:06:19 +0400

    BE: statistics

--- src/controllers/statistics.ts
+++ src/controllers/statistics.ts
@@ -1,6 +1,14 @@
 import {myRequest} from "../interfaces/General";
 import {NextFunction, Response} from "express";
 
-export async function getClosedOrdersData(req: myRequest, res: Response, next: NextFunction) {
+export async function getPricesTables(req: myRequest, res: Response, next: NextFunction) {
+
+}
+
+export async function getProductsTable(req: myRequest, res: Response, next: NextFunction) {
+
+}
+
+export async function getOrdersWaiter(req: myRequest, res: Response, next: NextFunction) {
 
 }
--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,4 +1,11 @@
 import * as express from "express";
+import {getPricesTables, getOrdersWaiter, getProductsTable} from "../controllers/statistics";
 const router = express.Router();
+
+router.get('/price/table', getPricesTables);
+
+router.get('/product/table', getProductsTable);
+
+router.get('/orders/waiter', getOrdersWaiter);
 
 export default router;
