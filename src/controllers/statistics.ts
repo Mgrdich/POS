1ca89fe9236aa3cc710dc2b15ed1560c26ec5c1c
Mgo <mgotm13@gmail.com>
2020-04-20 23:28:28 +0400

    BE: statistics

--- src/controllers/statistics.ts
+++ src/controllers/statistics.ts
@@ -1,14 +1,21 @@
-import {myRequest} from "../interfaces/General";
-import {NextFunction, Response} from "express";
+import {NextFunction, Response,Request} from "express";
+import {IProducts} from "../interfaces/models/Products";
+import {Products} from "../models/Products";
+import {noResult} from "../utilities/controllers/helpers";
 
-export async function getPricesTables(req: myRequest, res: Response, next: NextFunction) {
+export async function getProductsPrice(req: Request, res: Response, next: NextFunction) {
+    const productPrice:Array<IProducts> = await Products.find({},{name:1,price:1}).lean();
+    if(!productPrice.length){
+        return noResult(res);
+    }
+    res.status(200).json(productPrice);
+}
+
+export async function getOrdersPricesTables(req: Request, res: Response, next: NextFunction) {
 
 }
 
-export async function getProductsTable(req: myRequest, res: Response, next: NextFunction) {
 
-}
-
-export async function getOrdersWaiter(req: myRequest, res: Response, next: NextFunction) {
+export async function getOrdersWaiter(req: Request, res: Response, next: NextFunction) {
 
 }
--- src/routes/statistics.ts
+++ src/routes/statistics.ts
@@ -1,5 +1,11 @@
 import * as express from "express";
+import {getOrdersWaiter, getProductsPrice, getOrdersPricesTables} from "../controllers/statistics";
 const router = express.Router();
 
+router.get('/products/price', getProductsPrice);
+
+router.get('/orders/table', getOrdersPricesTables);
+
+router.get('/orders/waiter', getOrdersWaiter);
 
 export default router;
