5603901e726677c6598e6aaf9392b0050d3c97ea
Mgo <mgotm13@gmail.com>
2020-04-21 23:58:41 +0400

    FE: fixing the warnings

--- client/src/views/pos/TableOrdersButtons.tsx
+++ client/src/views/pos/TableOrdersButtons.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Button, Dialog, DialogActions, DialogContent, DialogTitle} from "@material-ui/core";
 import {deleteGroupAction, finishTableOrders, submitTableOrders} from "../../actions/posActions";
 import {useDispatch, useSelector} from "react-redux";
