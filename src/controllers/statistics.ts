b898d7d12c2f94624a73de308437893611f6ff87
Mgo <mgotm13@gmail.com>
2020-04-23 00:32:13 +0400

    BE: Table and price Fetching statistics

--- src/controllers/statistics.ts
+++ src/controllers/statistics.ts
@@ -18,11 +18,31 @@ export async function getProductsPrice(req: Request, res: Response, next: NextFu
  *
  * */
 export async function getClosedOrdersPricesTables(req: Request, res: Response, next: NextFunction) { //TODO add with date Filter
-    const closedOrders:Array<IClosedOrders> = await ClosedOrders.find({},{price:1,table:1}).populate('table','name');
+    const closedOrders:Array<IClosedOrders> = await ClosedOrders.find({},{price:1,table:1}).populate('table','number');
     if(!closedOrders.length){
         return noResult(res);
     }
-    res.status(200).json(closedOrders);
+
+    let orderHash:any = {
+     //tableId:arrayIndex
+    };
+
+    const getClosedOrdersPricesTables:Array<{price:number,number:number}> =
+        closedOrders.reduce(function (acc: Array<{ price: number, number: number }>, item: IClosedOrders, index: number) {
+            let arr: any = [...acc];
+            let tableId: string = item.table._id;
+            let tableNumber: number = item.table.number;
+            if (!orderHash[tableId] && orderHash[tableId] !== 0) { //ignore the index is zero  sum is not calculated
+                orderHash[tableId] = index; //hashing it
+                arr.push({number: tableNumber, price: item.price});
+                return arr;
+            }
+            let indexArray = orderHash[tableId];
+            arr[indexArray].price = arr[indexArray].price + item.price;
+            return arr;
+        }, []);
+
+    res.status(200).json(getClosedOrdersPricesTables);
 }
 
 
