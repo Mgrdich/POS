455e1b9164aaf3bbc58fe585b6a1cd4a4929edfc
sarkis1231 <sakooghly@gmail.com>
2020-04-23 22:33:08 +0400

    FE:code refactoring dashBoard

--- client/src/views/dashboard/index.tsx
+++ client/src/views/dashboard/index.tsx
@@ -3,20 +3,22 @@ import BarChart from "../../components/Reusable/Chart/BarChart";
 import Grid from "@material-ui/core/Grid";
 import {useFetch} from "../../components/Hooks/useFetch";
 import {Paper} from "@material-ui/core";
+import ComponentLoader from "../../components/Reusable/ComponentLoader";
 const Dashboard:React.FC = () => {
     const {data, isLoading} = useFetch('/statistics/products/price');
     const tickFormat = data.map((item:any) => item.name);
 
     return (
         <>
-            {isLoading ? <h1>Loading...</h1> :
-                <Grid container>
-                    <Grid  item xs={12} md={5}>
+            <ComponentLoader isLoading={isLoading}>
+                <Grid container direction='row'>
+                    <Grid item xs={12} md={8} lg={5}>
                         <Paper>
                             <BarChart data={data} x='name' y='price' tickFormat={tickFormat}/>
                         </Paper>
                     </Grid>
-                </Grid>}
+                </Grid>
+            </ComponentLoader>
         </>
     );
 };
