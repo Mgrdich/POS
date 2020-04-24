2e83f40e6f694f473420f509d8e2501a27282564
sarkis1231 <sakooghly@gmail.com>
2020-04-24 19:42:05 +0400

    FE: bug fixed for empty data

--- client/src/views/pos/index.tsx
+++ client/src/views/pos/index.tsx
@@ -6,6 +6,7 @@ import ComponentLoader from "../../components/Reusable/ComponentLoader";
 import {useHistory} from "react-router";
 import ErrorHandler from "../errors/ErrorHandler";
 import ChosenEmployee from "./ChosenEmployee";
+import CardMessage from "../../components/Reusable/CardMessage";
 
 const Pos: React.FC = () => {
     const tables: any = useSelector<any>(state => state.pos.tables.data);
@@ -33,13 +34,22 @@ const Pos: React.FC = () => {
                   wrap='wrap'>
                 <ErrorHandler error={error as boolean}>
                     <ComponentLoader isLoading={isLoading as boolean}>
-                        {tables.map((table: any) => (
-                            <div className={`tables ${table.status}`} key={table._id}>
-                                <Button key={table.id} onClick={() =>tableClick(table._id)}>
-                                    <span>{table.number}</span>
-                                </Button>
-                            </div>
-                        ))}
+                        {tables.empty ?
+                            <Grid item sm={12}>
+                            <CardMessage
+                                header='No tables create yet!'
+                                message='You can create tables by clicking on the button below'
+                                button={true}
+                                translation='Create tables'
+                                location='/tables'
+                            /> </Grid>:
+                            tables.map((table: any) => (
+                                <div className={`tables ${table.status}`} key={table._id}>
+                                    <Button key={table.id} onClick={() => tableClick(table._id)}>
+                                        <span>{table.number}</span>
+                                    </Button>
+                                </div>
+                            ))}
                     </ComponentLoader>
                 </ErrorHandler>
             </Grid>
