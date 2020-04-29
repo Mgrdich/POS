f5e46aeecb0b6288e6b463b0de52d0052f230447
hovaroyan <hovaroyan@gmail.com>
2020-04-30 01:41:37 +0400

    FE: constants folder and dateRanges file

---
+++ client/src/constants/dropDown/dateRanges.ts
@@ -0,0 +1,9 @@
+export const dateRanges = [
+    {value: 'today', placeholder: 'Today'},
+    {value: 'yesterday', placeholder: 'Yesterday'},
+    {value: 'this_month', placeholder: 'This month'},
+    {value: 'last_month', placeholder: 'Last month'},
+    {value: 'last_three_months', placeholder: 'Past three months'},
+    {value: 'ytd', placeholder: 'YTD'},
+    {value: 'beginning', placeholder: 'From beginning'}
+];
