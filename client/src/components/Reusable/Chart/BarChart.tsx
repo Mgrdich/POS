f35a957e0a31b422dc30183170847461fce097a3
sarkis1231 <sakooghly@gmail.com>
2020-05-03 13:34:57 +0400

    FE: code refactoring

--- client/src/components/Reusable/Chart/BarChart.tsx
+++ client/src/components/Reusable/Chart/BarChart.tsx
@@ -33,12 +33,12 @@ const BarChart: React.FC<IBarChart> = (props) => {
                 <VictoryAxis
                     tickValues={tickFormat}
                     tickFormat={tickFormat}
-                    style={{tickLabels: {fontSize: '10px', fill: '#66fcf1'}}}
+                    style={{tickLabels: {fontSize: '6px', fill: '#66fcf1'}}}
                 />
                 <VictoryAxis
                     dependentAxis
                     tickFormat={(x) => tickFormatFunction ? tickFormatFunction(x) : x}
-                    style={{tickLabels: {fontSize: '8px', fill: '#66fcf1'}}}
+                    style={{tickLabels: {fontSize: '6px', fill: '#66fcf1'}}}
 
                 />
                 <VictoryStack animate={{
@@ -47,7 +47,7 @@ const BarChart: React.FC<IBarChart> = (props) => {
                 }} colorScale={colorScale}>
                     <VictoryBar data={data} x={x} y={y}
                                 labels={({datum}) => labelsFunction ? labelsFunction(datum[labelsKey]) : datum[labelsKey]}
-                                style={{labels: {fontSize: '5px', color: '#1f2833'}}}
+                                style={{labels: {fontSize: '6px', color: '#1f2833'}}}
                                 labelComponent={
                                     <VictoryTooltip
                                         flyoutStyle={{fill: '#66fcf1', stroke: 'none'}}
